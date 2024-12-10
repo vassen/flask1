90c69612b5ab025e6045c946c952140d2b596424
Vasuliy M. <vassen@inbox.ru>
2024-12-10 21:22:18 +0300

    lab 1

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from flask import request
+from flask import g
 import sqlite3
 import sql_work
+from pathlib import Path
 
-connection = sqlite3.connect('flask.db',check_same_thread=False)
-cursor = connection.cursor()
+BASE_DIR = Path(__file__).parent
+DATABASE = BASE_DIR / "flask.db"  
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE,autocommit=True)
+    return db
+
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
@@ -14,40 +24,47 @@ def hello_world():
 
 @app.route("/quotes")
 def quotes_all():
+    cursor = get_db().cursor()
     data = sql_work.list_qoutes(cursor)
     return data
 
 @app.route("/quotes/<int:id>")
 def quotes_(id):
-      text = sql_work.get_quotes(cursor,id)
-      return text
+    cursor = get_db().cursor()
+    text = sql_work.get_quotes(cursor,id)
+    return text
 
 @app.route("/quotes", methods=['POST'])
 def create_quote():
+   cursor = get_db().cursor()
    quote = request.json
-   sql_work.create_quote(connection,cursor,quote)
+   sql_work.create_quote(cursor,quote)
    return quote, 201
 
 @app.route("/quotes/<int:id>", methods=['PUT'])
 def update_quote(id):
+   cursor = get_db().cursor()
    quote = request.json
    data = sql_work.update_quote(connection,cursor,quote,id)
    return data
 
 @app.route("/quotes/count")
 def count_quote():
+   cursor = get_db().cursor()
    lenght = sql_work.count_quotes(cursor)
    count={"count":lenght}
    return count
 
 @app.route("/quotes/random")
 def random():
+   cursor = get_db().cursor()
    data = sql_work.random_quote(cursor)
    print("data",data)
    return data
 
 @app.route("/quotes/<int:id>", methods=['DELETE'])
 def delete(id):
+   cursor = get_db().cursor()
    data = sql_work.delete_quote(connection,cursor,id)
    return data
 
--- sql_work.py
+++ sql_work.py
@@ -16,62 +16,53 @@ def random_quote(cursor):
    for id in data:
        ids.append(id[0])
    id = random.choice(ids)
-   print(id)
    cursor.execute('SELECT text FROM quotes WHERE id = ?', (id,))
    data = cursor.fetchall()[0][0]
    return data
 
 
-
 def get_quotes(cursor,id):
    try:
       cursor.execute(f'SELECT text FROM quotes where id={id}')
-      data = cursor.fetchall()
-      return data[0][0]
+      data = cursor.fetchone()
+      return data[0]
    except:
       return f"Quote with id={id} not found", 404
 
-def create_quote(connection,cursor,quote):
-   print(type(quote))
+def create_quote(cursor,quote):
    if quote.get("rating")==None or quote["rating"]>5 or quote["rating"]<1:
        quote["rating"]=1
    cursor.execute('SELECT max(id) FROM quotes')
-   last_id = cursor.fetchall()[0][0]+1
+   last_id = cursor.fetchone()[0]+1
    cursor.execute('INSERT INTO quotes (id, author, text, rating) VALUES (?, ?, ?, ?)', (last_id,str(quote["author"]),str(quote["text"]),str(quote["rating"])))
-   connection.commit()
-   print("data", quote)
 
 def count_quotes(cursor): 
-   cursor.execute('SELECT id FROM quotes')
-   data = cursor.fetchall()
-   count = len(data)
-   print(count)
-   return count
+   cursor.execute('SELECT count(id) FROM quotes')
+   data = cursor.fetchone()
+   return data
 
 
 def update_quote(connection,cursor,new_quote,id):
-    try:
-      sql_quote={}
-      cursor.execute(f'SELECT * FROM quotes where id={id}')
-      data = cursor.fetchall()[0]
-      print(data)
-      sql_quote["author"]=data[1]
-      sql_quote["text"]=data[2]
-      sql_quote.update(new_quote)
-      cursor.execute('UPDATE quotes set author = ?,text =? where id=? ', (str(sql_quote["author"]),str(sql_quote["text"]),id))
-      return sql_quote, 200
-    except:
-      return f"Quote with id={id} not found", 404
+   sql_quote={}
+   cursor.execute('SELECT * FROM quotes where id=?',(id,))
+   data = cursor.fetchall()[0]
+   if  new_quote["rating"]>5 or new_quote["rating"]<1:
+       new_quote.pop("rating")
+   sql_quote["author"]=data[1]
+   sql_quote["text"]=data[2]
+   sql_quote["rating"]=data[3]
+   sql_quote.update(new_quote)
+   cursor.execute('UPDATE quotes set author = ?,text =?, rating = ? where id=? ', (str(sql_quote["author"]),str(sql_quote["text"]), sql_quote["rating"],id))
+   return sql_quote, 200
+
+   return f"Quote with id={id} not found", 404
 
 def delete_quote(connection,cursor,id):
-   cursor.execute('SELECT id FROM quotes WHERE id = ?',(id,))
+   cursor.execute('DELETE FROM quotes WHERE id = ?', (id,))
    row = cursor.rowcount
-   data = cursor.fetchall()
-   print("data",data)
-   if not data:
-      return f"Quote with id={id} not found", 404
+   if not cursor.rowcount==0:
+       connection.commit()
+       return f"Quote with id {id} is deleted.", 200
    else:
-      cursor.execute('DELETE FROM quotes WHERE id = ?', (id,))
-      connection.commit()
-      return f"Quote with id {id} is deleted.", 200
+       return f"Quote with id {id} not found", 200
 
